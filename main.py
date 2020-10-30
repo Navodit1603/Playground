8e2a0ce6d7511ba4e04aeba4cc2a637c31a5f2c2
Navodit Maheshwari <navoditmaheshwari@gmail.com>
2020-10-30 16:09:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Navodit Maheshwari
+import math
+
+print("What is the base value?")
+b = int(input())
+print("What is the exponent?")
+e = int(input())
+print(b ** e) #The syntax for putting a value to the power of another is "**"
+
+print("Enter a number: ")
+a = int(input())  #User inputs value
+print(math.sqrt(a)) #prints square root
+
